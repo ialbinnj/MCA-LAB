1d02caf91bc35006ece405b29464ff156ee1b4d7
Albin N J <150993049+ialbinnj@users.noreply.github.com>
2024-04-22 16:06:28 +0530

    Java Pro24

---
+++ S2/Java/Pro24.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Pro24 {
+    public static void main(String[] args) {
+        int i,j,temp;
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the Array Elements : ");
+        for(i=0;i<n;i++){
+            arr[i] = scn.nextInt();
+        }
+        System.out.println("Array is : ");
+        for(i=0;i<n;i++){
+            System.out.print(arr[i]+"\t");
+        }
+        for(i=0;i<n-1;i++){
+            for(j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("\nSorted Array is : ");
+        for(i=0;i<n;i++){
+            System.out.print(arr[i]+"\t");
+        }
+    }
+}
