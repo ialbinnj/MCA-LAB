aad694d70b96af902c25e125371c906beab6b6b0
Albin N J <150993049+ialbinnj@users.noreply.github.com>
2024-05-02 14:34:34 +0530

    Java Pro17

--- S2/Java/Pro17.java
+++ S2/Java/Pro17.java
@@ -1,16 +1,29 @@
-import GraphicS.Area;
 import java.util.*;
 
-class Pro17{
-    public static void main(String[] args){
-        Area a = new Area();
-        System.out.println("Rectangle: ");
-        a.rectangle();
-        System.out.println("Square: ");
-        a.square();
-        System.out.println("Triangle: ");
-        a.triangle();
-        System.out.println("Circle: ");
-        a.circle();
-    }
+class AuthException extends Exception{
+   public AuthException(String input){
+      super(input);
+   }
+}
+
+public class Pro17{
+   public static void main(String args[]){
+      Scanner input = new Scanner(System.in);
+      String userName = "MBITS";
+      String pwd = "mbits";
+      try{
+        System.out.print("Enter UserName: ");
+        String nameInput = input.next();
+        System.out.print("Enter Password: ");
+        String passwordInput = input.next();
+        
+        if(nameInput.equals(userName) && passwordInput.equals(pwd)){
+           throw new AuthException("Valid User");
+        }else{
+           throw new AuthException("Not Valid User");
+        }
+      }catch(AuthException e){
+         System.out.println(e.getMessage());
+      }
+   }
 }
