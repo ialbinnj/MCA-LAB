b9fc008c7b737a1bca9101654fb3728685d49cba
Albin N J <150993049+ialbinnj@users.noreply.github.com>
2024-05-04 23:33:36 +0530

    Java Pro18

--- S2/Java/Pro18.java
+++ S2/Java/Pro18.java
@@ -1,19 +1,39 @@
-import Arithmetic.ArithmeticOperations;
 import java.util.*;
 
-class Pro18{
-    public static void main(String[] args){
-        ArithmeticOperations a = new ArithmeticOperations();
-        
-        Scanner scn = new Scanner(System.in);
-        System.out.print("Enter the Num1 : ");
-        float num1 = scn.nextFloat();
-        System.out.print("Enter the Num2 : ");
-        float num2 = scn.nextFloat();
-        
-        a.add(num1,num2);
-        a.sub(num1,num2);
-        a.mult(num1,num2);
-        a.divide(num1,num2);
+class CheckException extends Exception{
+   public CheckException(String input){
+      super(input);
+   }
+}
+
+public class Pro18 {
+    public static void main(String[] args) {
+        double sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Count of numbers");
+        int N = sc.nextInt();
+        int count=0;
+        int[] numbers = new int[N];
+        for(int i =0;i<N; i++){
+            System.out.println("Enter the number");
+            numbers[i] = sc.nextInt();
+        }
+        for(int i =0;i<N; i++){
+            try {
+                if (numbers[i] >= 0) {
+                    sum += numbers[i];
+                    count+=1;
+                } else {
+                    throw new CheckException("Negative number : " +numbers[i]);
+                }
+            }
+            catch (CheckException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        double avg = sum/count;
+        System.out.println("Average of Positive Numbers ="+avg);
+
     }
+
 }
