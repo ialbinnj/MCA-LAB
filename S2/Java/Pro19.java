e5ac02a1869105bb5bff0e5f1710e75a951f4f90
Albin N J <150993049+ialbinnj@users.noreply.github.com>
2024-05-04 23:34:52 +0530

    Java Pro19

--- S2/Java/Pro19.java
+++ S2/Java/Pro19.java
@@ -1,29 +1,54 @@
-import java.util.*;
+import java.util.Scanner;
 
-class AuthException extends Exception{
-   public AuthException(String input){
-      super(input);
-   }
+class Mult extends Thread{
+    int n;
+    Mult(int n){
+        this.n = n;
+    }
+    public void run(){
+        for(int i=1;i<=this.n;i++){
+            try{
+                System.out.println("5"+"x"+i+"="+(i*5));
+                sleep(1000);
+            }catch(Exception e){}
+        }
+    }
+}
+
+class Prime extends Thread{
+    int num;
+    Prime(int num){
+        this.num = num;
+    }
+    public void run(){
+        for(int i=2;i<=this.num;i++){
+            int f = 0;
+            for(int j=2;j<=i/2;j++){
+                if(i%j==0){
+                    f=1;
+                    break;
+                }
+            }
+            if(f==0){
+                try{
+                    System.out.println(i+" is a prime Number");
+                    sleep(1000);
+                }catch(Exception e){}
+            }
+        }
+    }
 }
 
 public class Pro19{
-   public static void main(String args[]){
-      Scanner input = new Scanner(System.in);
-      String userName = "MBITS";
-      String pwd = "mbits";
-      try{
-        System.out.print("Enter UserName: ");
-        String nameInput = input.next();
-        System.out.print("Enter Password: ");
-        String passwordInput = input.next();
-        
-        if(nameInput.equals(userName) && passwordInput.equals(pwd)){
-           throw new AuthException("Valid User");
-        }else{
-           throw new AuthException("Not Valid User");
-        }
-      }catch(AuthException e){
-         System.out.println(e.getMessage());
-      }
-   }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter the Limit for Multiplication Table : ");
+        int n = scn.nextInt();
+        System.out.print("Enter the Limit for Prime Numbers : ");
+        int m = scn.nextInt();
+        Mult m1 = new Mult(n);
+        Prime p1 = new Prime(m);
+        m1.start();
+        p1.start();
+    }
 }
