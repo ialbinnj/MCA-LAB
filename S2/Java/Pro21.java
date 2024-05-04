f1914876254a7868edb9677a93d1996248337e70
Albin N J <150993049+ialbinnj@users.noreply.github.com>
2024-05-04 23:40:35 +0530

    Java Pro21

--- S2/Java/Pro21.java
+++ S2/Java/Pro21.java
@@ -1,54 +1,70 @@
 import java.util.Scanner;
 
-class Mult extends Thread{
-    int n;
-    Mult(int n){
+class Stack{
+    int n,top=-1;
+    int a[] = new int[50];
+    Scanner scn = new Scanner(System.in);
+    
+    Stack(int n){
         this.n = n;
     }
-    public void run(){
-        for(int i=1;i<=this.n;i++){
-            try{
-                System.out.println("5"+"x"+i+"="+(i*5));
-                sleep(1000);
-            }catch(Exception e){}
+    
+    void push(){
+        if(top>=n-1){
+            System.out.println("Stack is Full");
+        }else{
+            System.out.print("Enter the Element to push : ");
+            int x = scn.nextInt();
+            top=top+1;
+            a[top] = x;
+            System.out.println(x+" is Inserted");
+        }
+    }
+    
+    void pop(){
+        if(this.top==-1){
+            System.out.println("Stack is Empty");
+        }else{
+            System.out.println(a[top]+" is Deleted");
+            top=top-1;
+        }
+    }
+    
+    void disp(){
+        if(this.top==-1){
+            System.out.println("Stack is Empty");
+        }else{
+            for(int i=0;i<=top;i++){
+                System.out.print(a[i]+"\t");
+            }
+            System.out.println();
         }
     }
 }
 
-class Prime extends Thread{
-    int num;
-    Prime(int num){
-        this.num = num;
-    }
-    public void run(){
-        for(int i=2;i<=this.num;i++){
-            int f = 0;
-            for(int j=2;j<=i/2;j++){
-                if(i%j==0){
-                    f=1;
-                    break;
-                }
-            }
-            if(f==0){
-                try{
-                    System.out.println(i+" is a prime Number");
-                    sleep(1000);
-                }catch(Exception e){}
-            }
-        }
-    }
-}
-
-public class Pro21{
-    public static void main(String[] args){
+public class Pro21 {
+    public static void main(String[] args) {
+        int n;
         Scanner scn = new Scanner(System.in);
-        System.out.print("Enter the Limit for Multiplication Table : ");
-        int n = scn.nextInt();
-        System.out.print("Enter the Limit for Prime Numbers : ");
-        int m = scn.nextInt();
-        Mult m1 = new Mult(n);
-        Prime p1 = new Prime(m);
-        m1.start();
-        p1.start();
+        System.out.print("Enter the size of the Stack : ");
+        n = scn.nextInt();
+        Stack stack1 = new Stack(n);
+        while(true){
+            System.out.println("1.Push\n2.Pop\n3.Display\n4.Exit");
+            System.out.print("Enter Your Choice : ");
+            int ch = scn.nextInt();
+            switch(ch){
+                case 1: stack1.push();
+                        break;
+                case 2: stack1.pop();
+                        break;
+                case 3: stack1.disp();
+                        break;
+                case 4: System.exit(0);
+                        break;
+                default:System.out.println("Enter a valid Choice");
+                        break;
+            }
+        }
     }
 }
