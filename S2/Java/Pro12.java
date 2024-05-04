146ff5d8baaa7b5247ca650570934b4340fb2fc7
Albin N J <150993049+ialbinnj@users.noreply.github.com>
2024-05-05 00:16:17 +0530

    Java Pro12

--- S2/Java/Pro12.java
+++ S2/Java/Pro12.java
@@ -1,89 +1,84 @@
 import java.util.Scanner;
-class Person{
-    String name;
-    String gender;
-    String address;
-    int age;
-    Person(String name,String gender,String address,int age){
-        this.name = name;
-        this.gender = gender;
-        this.address = address;
-        this.age = age;
+class Publisher{
+    String pub_name;
+    Publisher(String pub_name){
+        this.pub_name = pub_name;
     }
 }
-class Employee extends Person{
-    int empid;
-    String company_name;
-    String qualification;
-    double salary;
-    Employee(String name,String gender,String address,int age,int empid,String company_name,String qualification,double salary){
-        super(name,gender,address,age);
-        this.empid = empid;
-        this.company_name = company_name;
-        this.qualification = qualification;
-        this.salary = salary;
+class Book extends Publisher{
+    String book_name;
+    String author;
+    float price;
+    Book(String pub_name,String book_name,String author,float price){
+        super(pub_name);
+        this.book_name = book_name;
+        this.author = author;
+        this.price = price;
     }
 }
-class Teacher extends Employee{
-    String subject;
-    String department;
-    int teacherid;
-    Teacher(String name,String gender,String address,int age,int empid,String company_name,String qualification,double salary,String subject,String department,int teacherid){
-        super(name,gender,address,age,empid,company_name,qualification,salary);
-        this.subject = subject;
-        this.department = department;
-        this.teacherid = teacherid;
+class Literature extends Book{
+    Literature(String pub_name,String book_name,String author,float price){
+        super(pub_name,book_name,author,price);
     }
     void disp(){
-        System.out.println("Name : "+name);
-        System.out.println("Gender : "+gender);
-        System.out.println("Address : "+address);
-        System.out.println("Age : "+age);
-        System.out.println("EmpId : "+empid);
-        System.out.println("Company Name : "+company_name);
-        System.out.println("Qualification : "+qualification);
-        System.out.println("Salary : "+salary);
-        System.out.println("Subject : "+subject);
-        System.out.println("Department : "+department);
-        System.out.println("Teacher Id : "+teacherid);
+        System.out.println("Publisher Name : "+pub_name);
+        System.out.println("Book Name : "+book_name);
+        System.out.println("Author : "+author);
+        System.out.println("Price : "+price);
     }
 }
-public class Pro12 {
-    public static void main(String[] args) {
+class Fiction extends Book{
+    Fiction(String pub_name,String book_name,String author,float price){
+        super(pub_name,book_name,author,price);
+    }
+    void disp(){
+        System.out.println("Publisher Name : "+pub_name);
+        System.out.println("Book Name : "+book_name);
+        System.out.println("Author : "+author);
+        System.out.println("Price : "+price);
+    }
+}
+class BookDetails {
+    static String t1,t2,t3;
+    static float t4;
+    static Scanner scn = new Scanner(System.in).useDelimiter("\n");
+    static void read(){
+        System.out.print("Publisher Name : ");
+        t1 = scn.next();
+        System.out.print("Book Name : ");
+        t2 = scn.next();
+        System.out.print("Author : ");
+        t3 = scn.next();
+        System.out.print("Price : ");
+        t4 = scn.nextFloat();
+    }
+    public static void main(String args[]){
         int i;
-        Scanner scn = new Scanner(System.in);
-        Teacher teacher[] = new Teacher[10];
-        System.out.print   ("Enter total no of Teachers : ");
+        Literature lit[] = new Literature[10];
+        Fiction fic[] = new Fiction[10];
+        System.out.print("Enter the no. of Literature Books : ");
         int n = scn.nextInt();
         for(i=0;i<n;i++){
-            System.out.println("\nEnter the details of teacher "+(i+1));
-            System.out.print("Name : ");
-            String t1 = scn.next();
-            System.out.print("Gender : ");
-            String t2 = scn.next();
-            System.out.print("Address : ");
-            String t3 = scn.next();
-            System.out.print("Age : ");
-            int t4 = scn.nextInt();
-            System.out.print("EmpId : ");
-            int t5 = scn.nextInt();
-            System.out.print("Company Name : ");
-            String t6 = scn.next();
-            System.out.print("Qualification : ");
-            String t7 = scn.next();
-            System.out.print("Salary : ");
-            double t8 = scn.nextDouble();
-            System.out.print("Subject : ");
-            String t9 = scn.next();
-            System.out.print("Department : ");
-            String t10 = scn.next();
-            System.out.print("Teacher Id : ");
-            int t11 = scn.nextInt();
-            teacher[i] = new Teacher(t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11);
+            System.out.println("\nEnter the Details of Book "+(i+1));
+            read();
+            lit[i] = new Literature(t1,t2,t3,t4);
         }
+        System.out.print("Enter the no. of Fiction Books : ");
+        int m = scn.nextInt();
+        for(i=0;i<m;i++){
+            System.out.println("\nEnter the Details of Book "+(i+1));
+            read();
+            fic[i] = new Fiction(t1,t2,t3,t4);
+        }
+        System.out.println("\nDetails of Literature Books ");
         for(i=0;i<n;i++){
-            System.out.println("\nDetails of teacher "+(i+1));
-            teacher[i].disp();
+            System.out.println("\nBook "+(i+1));
+            lit[i].disp();
+        }
+        System.out.println("\nDetails of Fiction Books ");
+        for(i=0;i<m;i++){
+            System.out.println("\nBook "+(i+1));
+            fic[i].disp();
         }
     }
 }
